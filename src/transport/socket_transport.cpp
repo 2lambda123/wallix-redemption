kikokikok/redemption

SocketTransport: remove error_message parameter, the context is in Error

src/transport/socket_transport.cpp
@@ -60,12 +60,11 @@ SocketTransport::SocketTransport(
     std::chrono::milliseconds connection_establishment_timeout,
     std::chrono::milliseconds tcp_user_timeout,
     std::chrono::milliseconds recv_timeout,
-    Verbose verbose, std::string * error_message
+    Verbose verbose
 )
     : sck(sck.release())
     , name(name.name)
     , port(port)
-    , error_message(error_message)
     , tls(nullptr)
     , recv_timeout(recv_timeout)
     , connection_establishment_timeout(connection_establishment_timeout)
@@ -133,13 +132,13 @@ Transport::TlsResult SocketTransport::enable_client_tls(ServerNotifier & server_
         case TLSState::Uninit:
             LOG(LOG_INFO, "Client TLS start");
             this->tls = std::make_unique<TLSContext>();
-            if (!this->tls->enable_client_tls_start(this->sck, this->error_message, tls_config)) {
+            if (!this->tls->enable_client_tls_start(this->sck, tls_config)) {
                 return Transport::TlsResult::Fail;
             }
             this->tls_state = TLSState::Want;
             [[fallthrough]];
         case TLSState::Want: {
-            Transport::TlsResult ret = this->tls->enable_client_tls_loop(this->error_message);
+            Transport::TlsResult ret = this->tls->enable_client_tls_loop();
             switch (ret) {
                 case TlsResult::Fail:
                     this->tls.reset();

2lambda123/wallix-redemption

src/transport/socket_transport.cpp
@@ -49,12 +49,11 @@ SocketTransport::SocketTransport(
     std::chrono::milliseconds connection_establishment_timeout,
     std::chrono::milliseconds tcp_user_timeout,
     std::chrono::milliseconds recv_timeout,
-    Verbose verbose, std::string * error_message
+    Verbose verbose
 )
     : sck(sck.release())
     , name(name.name)
     , port(port)
-    , error_message(error_message)
     , tls(nullptr)
     , recv_timeout(recv_timeout)
     , connection_establishment_timeout(connection_establishment_timeout)
@@ -123,13 +122,13 @@ Transport::TlsResult SocketTransport::enable_client_tls(ServerNotifier & server_
         case TLSState::Uninit:
             LOG(LOG_INFO, "Client TLS start");
             this->tls = std::make_unique<TLSContext>();
-            if (!this->tls->enable_client_tls_start(this->sck, this->error_message, tls_client_params)) {
+            if (!this->tls->enable_client_tls_start(this->sck, tls_client_params)) {
                 return Transport::TlsResult::Fail;
             }
             this->tls_state = TLSState::Want;
             [[fallthrough]];
         case TLSState::Want: {
-            Transport::TlsResult ret = this->tls->enable_client_tls_loop(this->error_message);
+            Transport::TlsResult ret = this->tls->enable_client_tls_loop();
             switch (ret) {
                 case TlsResult::Fail:
                     this->tls.reset();