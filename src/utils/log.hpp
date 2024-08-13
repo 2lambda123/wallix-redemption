79fe91e0546a724750aef5182bcacb5f80615802
Jonathan Poelen <jpoelen@wallix.com>
2024-08-13 17:17:30 +0200

    simplify LOG() and remove dependency to boost.preprocessor

--- src/utils/log.hpp
+++ src/utils/log.hpp
@@ -79,76 +79,11 @@ auto log_value(T const & x) noexcept
 -> decltype(detail_::log_value(x, 1))
 { return detail_::log_value(x, 1); }
 
-
-#if ! defined(IN_IDE_PARSER) && REDEMPTION_HAS_INCLUDE(<boost/preprocessor/config/config.hpp>)
-# include <boost/preprocessor/config/config.hpp>
-
-# if BOOST_PP_VARIADICS
-
-#  include <boost/preprocessor/cat.hpp>
-#  include <boost/preprocessor/comparison/greater.hpp>
-#  include <boost/preprocessor/array/pop_front.hpp>
-#  include <boost/preprocessor/array/pop_back.hpp>
-#  include <boost/preprocessor/array/to_list.hpp>
-#  include <boost/preprocessor/array/elem.hpp>
-#  include <boost/preprocessor/list/for_each.hpp>
-#  include <boost/preprocessor/variadic/elem.hpp>
-#  include <boost/preprocessor/variadic/to_list.hpp>
-
-#  define REDEMPTION_LOG_VALUE_PARAM_0(elem)
-#  define REDEMPTION_LOG_VALUE_PARAM_1(elem) , log_value(elem).value()
-
-// Boost.Preprocessor 1.75
-// The beginning 'r' and 'd' iteration numbers in the user-defined macros start at 1 and not 2
-#  ifdef BOOST_PP_IS_STANDARD
-#    define REDEMPTION_LOG_VALUE_PARAM(r, data, elem) \
-       BOOST_PP_CAT(REDEMPTION_LOG_VALUE_PARAM_, BOOST_PP_BOOL(BOOST_PP_GREATER(r, 1)))(elem)
-#  else
-#    define REDEMPTION_LOG_VALUE_PARAM(r, data, elem) \
-       BOOST_PP_CAT(REDEMPTION_LOG_VALUE_PARAM_, BOOST_PP_BOOL(BOOST_PP_GREATER(r, 2)))(elem)
-#  endif
-
-#  define LOG_REDEMPTION_VARIADIC_TO_LOG_PARAMETERS(...) \
-    "" BOOST_PP_VARIADIC_ELEM(0, __VA_ARGS__)            \
-    BOOST_PP_LIST_FOR_EACH(                              \
-        REDEMPTION_LOG_VALUE_PARAM,                      \
-        BOOST_PP_NIL,                                    \
-        BOOST_PP_VARIADIC_TO_LIST(__VA_ARGS__)           \
-    )
-
-// checked by the compiler
-#  define LOG_REDEMPTION_FORMAT_CHECK(...)                     \
-    void(sizeof(printf(                                        \
-        LOG_REDEMPTION_VARIADIC_TO_LOG_PARAMETERS(__VA_ARGS__) \
-    )))
-
-#  define REDEMPTION_LOG_VALUE(x) (x)
-
-# endif
-#endif
-
-#ifndef LOG_REDEMPTION_FORMAT_CHECK
-
-# ifndef REDEMPTION_DISABLE_NO_BOOST_PREPROCESSOR_WARNING
-#   if defined __GNUC__ || defined __clang__
-#       pragma GCC warning "Cannot check format in 'LOG' (no boost preprocessor headers or unsupported variadic macro). Disable this warning with -DREDEMPTION_DISABLE_NO_BOOST_PREPROCESSOR_WARNING."
-#   else
-#       warning "Cannot check format in 'LOG' (no boost preprocessor headers or unsupported variadic macro)"
-#   endif
-# endif
-
-# define LOG_REDEMPTION_FORMAT_CHECK(...) ::compiler_aux_::unused_variables(__VA_ARGS__)
-# define REDEMPTION_LOG_VALUE(x) log_value(x).value()
-# define LOG_REDEMPTION_VARIADIC_TO_LOG_PARAMETERS(...) __VA_ARGS__
-
-# define LOG_UNCHECKED_FORMAT 1
-
-#endif
-
-
 #ifdef IN_IDE_PARSER
-# define LOG(priority, ...) ::compiler_aux_::unused_variables(priority, "" __VA_ARGS__)
-# define LOG_UNCHECKED_FORMAT 1
+# define LOG(priority, format, ...)                 \
+    [&](auto const&... elem){                       \
+        printf("" format, log_value(elem).value()); \
+    }(__VA_ARGS__)
 
 #else
 # ifdef NDEBUG
@@ -166,36 +101,18 @@ auto log_value(T const & x) noexcept
     REDEMPTION_DIAGNOSTIC_POP()
 # endif
 
-# define LOG(priority, ...) do {                                     \
-    using ::log_value;                                               \
-    LOG_REDEMPTION_FILENAME(priority)                                \
-    ::detail::LOGCHECK_REDEMPTION_INTERNAL((                         \
-        LOG_REDEMPTION_FORMAT_CHECK(__VA_ARGS__),                    \
-        ::detail::LOG_REDEMPTION_INTERNAL(priority, "%s (%d/%d) -- " \
-            LOG_REDEMPTION_VARIADIC_TO_LOG_PARAMETERS(__VA_ARGS__)), \
-        1                                                            \
-    ));                                                              \
- } while (0)
-
-namespace detail
-{
-    inline void LOGCHECK_REDEMPTION_INTERNAL(int /*unused*/) noexcept
-    {}
-}
+# define LOG(priority, format, ...) [&](auto const&... elem){    \
+    using ::log_value;                                           \
+    LOG_REDEMPTION_FILENAME(priority)                            \
+    void(sizeof(printf("" format, log_value(elem).value()...))), \
+    ::detail::LOG_REDEMPTION_INTERNAL(priority, "%s (%d/%d) -- " \
+        format, log_value(elem).value()...);                     \
+ }(__VA_ARGS__)
 #endif
 
 #define LOG_IF(cond, priority, ...)                                              \
     if (REDEMPTION_UNLIKELY(cond)) /*NOLINT(readability-simplify-boolean-expr)*/ \
         LOG(priority, __VA_ARGS__)
-
-#if defined(LOG_UNCHECKED_FORMAT)
-namespace compiler_aux_
-{
-    template<class... Ts>
-    void unused_variables(Ts const & ...) noexcept
-    {}
-}
-#endif
 
 void LOG_REDEMPTION_INTERNAL_IMPL(int priority, char const * format, ...) noexcept;
 
@@ -225,7 +142,6 @@ namespace detail
     template<class... Ts>
     void LOG_REDEMPTION_INTERNAL(int priority, char const * format, Ts const & ... args) noexcept
     {
-        using ::log_value;
         int const pid = getpid();
         LOG_REDEMPTION_INTERNAL_IMPL(
             priority,
@@ -233,7 +149,7 @@ namespace detail
             prioritynames[priority],
             pid,
             pid,
-            REDEMPTION_LOG_VALUE(args)...
+            args...
         );
     }
 } // namespace detail
